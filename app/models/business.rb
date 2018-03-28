e90d9fa46a060a237bf2815b8deeff3b44147a56
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-03-28 19:32:26 -0400

    Add ActiveRecord associations in all models

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,7 @@
 class Business < ApplicationRecord
-
+  belongs_to :category
+  has_many :items
+  has_many :reviews
+  has_many :ratings, through: :reviews
+  has_many :users, through: :reviews
 end
