84eb8d8fcc57819fa0a1f30a2669f3eb7cdde12f
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-03-29 19:57:52 -0400

    Fix Sign Out
    Add :method => 'delete' to destroy_user_session_path and delete redirect_to in after_sign_out_path_for method fixes the inability to sign out properly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   # Overwriting the users/sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
-    redirect_to root_path
+    root_path
   end
 end
