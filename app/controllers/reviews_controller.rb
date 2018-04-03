9afa341a4a3a4b609bb8be3adb6deb861b838f33
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-04-03 13:01:04 -0400

    Add before_action :set_business to ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,15 @@
 class ReviewsController < ApplicationController
   before_action :require_login, except: [:show, :index]
+  before_action :set_business
 
   def new
-    @business = Business.find_by(id: params[:business_id])
     @review = Review.new
   end
 
   def create
     review = Review.new(review_params)
-    if review.save && params[:business_id]
-      redirect_to biz_review_path(params[:business_id], review)
+    if review.save && !!@business
+      redirect_to biz_review_path(@business, review)
     elsif review.save
       redirect_to review_path(review)
     else
@@ -21,7 +21,14 @@ class ReviewsController < ApplicationController
     @review = Review.find_by(id: params[:id])
   end
 
+  def index
+  end
+
   private
+
+  def set_business
+    @business = Business.find_by(id: params[:business_id])
+  end
 
   def review_params
     params.require(:review).permit(:title, :comment, :would_recommend)
