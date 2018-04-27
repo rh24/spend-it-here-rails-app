64dcb3fe6d191f01e67fd667287a88d22dff1d6b
rh24 <ms.rebeccahong@gmail.com>
2018-04-27 15:10:48 -0400

    Update crypto.rb

--- app/models/crypto.rb
+++ app/models/crypto.rb
@@ -5,7 +5,7 @@ class Crypto < ApplicationRecord
   # Right now, the Crypto model seeded data is ordered by the rank in which the data was first scraped.
   # In the future, I want to be able to add dropdown menus that display the Cryptos in order by name.
   # Or have a typing text_field that provides suggestions for matches.
-  # I don't want false data and coins that don't exist to be created, so I felt it was safer to user data from a scraper.
+  # I don't want false data and coins that don't exist to be created, so I felt it was safer to use data from a scraper.
 
   # def self.order_by_name
   #   order(name: "asc")
